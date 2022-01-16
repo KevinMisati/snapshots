1f578fc429aac55ba82371f72e53e0bd204902f1
kevinmisati <kevinmosigisi2001@gmail.com>
2022-01-16 08:39:47 +0300

    finally it is working

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ import DispalyImages from "./Components/DisplayImages"
 function App () {
   const [query,setQuery] = useState("food")
   const [images,setImages] = useState([])
+  const [loading,setLoading] = useState(false)
  
   const getCategoryQuery = (categoryQuery) => {
     setQuery(categoryQuery)
   }
   const fetchData = async () => {
+    setLoading(true)
     const response = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=020fada4ee15a8b88e543fb1552ac0ac&tags=${query}&per_page=24&format=json&nojsoncallback=1`);
     const responseData = await response.json();
 
     setImages(responseData.photos.photo)
+    setLoading(false)
   } 
 
   useEffect(() => {
@@ -30,7 +33,7 @@ function App () {
       <Header />
       <SnapForm getCategoryQuery={getCategoryQuery} />
       <Categories getCategoryQuery={getCategoryQuery} />
-      <DispalyImages query={query} imgs={images}   />
+      <DispalyImages isLoading={loading} query={query} imgs={images}   />
      
     </div>
   );
--- src/Components/DisplayImages.js
+++ src/Components/DisplayImages.js
@@ -2,11 +2,12 @@ import React,{useContext} from 'react'
 import classes from "./DisplayImages.module.css"
 import SingleImage from "./SingleImage"
 
-const DisplayImages = ({query,imgs}) => {
+const DisplayImages = ({query,imgs,isLoading}) => {
 
     return (
         <div className={classes["images-container"]}>
             <h2>{query} Images</h2>
+            {isLoading && <h4>Loading...</h4>}
             <div className={classes.images}>
                 {imgs.map((image) => {
                     return <SingleImage image={image} />
